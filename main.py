f4961ee0086b34b1a7f410e2a6e28ab49bbcd62b
Александр <98910740+finake0@users.noreply.github.com>
2023-02-24 22:48:40 +0200

    added new file

---
+++ main.py
@@ -0,0 +1,38 @@
+import re
+import aiogram
+import requests
+from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, executor, types
+
+bot_token = ''
+
+bot = Bot(token=bot_token)
+
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Отправь мне гос.номер автомобиля 🇺🇦")
+
+@dp.message_handler(lambda message: re.search(r'^[А-Я]{2}\d{4}[А-Я]{2}$', message.text))
+async def search_car(message: types.Message):
+    car_number = message.text.upper()
+
+    key = ""
+    url = f"https://baza-gai.com.ua/nomer/{car_number}"
+    headers = {"Accept": "application/json", "X-Api-Key": key}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        car_info = f"Номер: {data['digits']}\n"
+        car_info += f"Марка: {data['vendor']} {data['model']}\n"
+        car_info += f"Год выпуска: {data['model_year']}\n"
+        car_info += f"Регион: {data['region']['name']} ({data['region']['name_ua']})\n"
+        car_info += f"Цвет: {data['operations'][0]['color']['ru']} ({data['operations'][0]['color']['ua']})\n"
+        await message.answer(car_info)
+    else:
+        await message.answer("Ошибка!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
